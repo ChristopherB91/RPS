1d458dd53d02394629f49094db49d85725a0fd2c
Chris <chrisbrowne91@gmail.com>
2022-11-19 06:00:52 -0500

    Game is first to five now

--- Script.js
+++ Script.js
@@ -1,5 +1,5 @@
-let playerScore = 0;
-let compScore = 0;
+var playerScore = 0;
+var compScore = 0;
 
 function play(num) {
 const Selections = [
@@ -18,7 +18,7 @@ const Selections = [
 ]
 
 function computerSelection() {
-    let compC = Math.floor(Math.random() * 3);
+    let compC = Math.floor(Math.random() * 2);
     return Selections[compC]
 }
 
@@ -27,55 +27,65 @@ function playerSelection(nums) {
 }
 
 function isWinner(selection, opponent){
-    if(selection === opponent){
-        document.querySelector('#results').innerHTML = 'DRAW'
-        document.querySelector('#pChoice').innerHTML = selection.name
-        document.querySelector('#cChoice').innerHTML = opponent.name
-    }
-    else if(selection.name == 'rock'){
-        if(opponent.name == 'paper'){
-        compScore++
-        document.querySelector('#comp').innerHTML = 'Computer Score:' + ' ' + compScore
-        document.querySelector('#results').innerHTML = 'LOOSE'
-        document.querySelector('#pChoice').innerHTML = selection.name
-        document.querySelector('#cChoice').innerHTML = opponent.name
-        }else{
-        playerScore++
-        document.querySelector('#player').innerHTML = 'Player Score:' + ' ' + playerScore
-        document.querySelector('#results').innerHTML = 'WIN'
-        document.querySelector('#pChoice').innerHTML = selection.name
-        document.querySelector('#cChoice').innerHTML = opponent.name
+    if(playerScore < 5 && compScore < 5){
+        if(selection === opponent){
+            document.querySelector('#results').innerHTML = 'DRAW'
+            document.querySelector('#pChoice').innerHTML = selection.name
+            document.querySelector('#cChoice').innerHTML = opponent.name
+        }
+        else if(selection.name == 'rock'){
+            if(opponent.name == 'paper'){
+            compScore++
+            document.querySelector('#comp').innerHTML = 'Computer Score:' + ' ' + compScore
+            document.querySelector('#results').innerHTML = 'LOSE'
+            document.querySelector('#pChoice').innerHTML = selection.name
+            document.querySelector('#cChoice').innerHTML = opponent.name
+            }else{
+            playerScore++
+            document.querySelector('#player').innerHTML = 'Player Score:' + ' ' + playerScore
+            document.querySelector('#results').innerHTML = 'WIN'
+            document.querySelector('#pChoice').innerHTML = selection.name
+            document.querySelector('#cChoice').innerHTML = opponent.name
+            }
+        }
+        else if(selection.name == 'scissors'){
+            if(opponent.name == 'rock'){
+            compScore++
+            document.querySelector('#comp').innerHTML = 'Computer Score:' + ' ' + compScore
+            document.querySelector('#results').innerHTML = 'LOOSE'
+            document.querySelector('#pChoice').innerHTML = selection.name
+            document.querySelector('#cChoice').innerHTML = opponent.name
+            }else{
+            playerScore++
+            document.querySelector('#player').innerHTML = 'Player Score:' + ' ' + playerScore
+            document.querySelector('#results').innerHTML = 'WIN'
+            document.querySelector('#pChoice').innerHTML = selection.name
+            document.querySelector('#cChoice').innerHTML = opponent.name
+            }
+        }
+        else if(selection.name == 'paper'){
+            if(opponent.name == 'scissors'){
+            compScore++
+            document.querySelector('#comp').innerHTML = 'Computer Score:' + ' ' + compScore
+            document.querySelector('#results').innerHTML = 'LOOSE'
+            document.querySelector('#pChoice').innerHTML = selection.name
+            document.querySelector('#cChoice').innerHTML = opponent.name
+            }else{
+            playerScore++
+            document.querySelector('#player').innerHTML = 'Player Score:' + ' ' + playerScore
+            document.querySelector('#results').innerHTML = 'WIN'
+            document.querySelector('#pChoice').innerHTML = selection.name
+            document.querySelector('#cChoice').innerHTML = opponent.name
+            }
         }
     }
-    else if(selection.name == 'scissors'){
-        if(opponent.name == 'rock'){
-        compScore++
-        document.querySelector('#comp').innerHTML = 'Computer Score:' + ' ' + compScore
-        document.querySelector('#results').innerHTML = 'LOOSE'
-        document.querySelector('#pChoice').innerHTML = selection.name
-        document.querySelector('#cChoice').innerHTML = opponent.name
-        }else{
-        playerScore++
-        document.querySelector('#player').innerHTML = 'Player Score:' + ' ' + playerScore
-        document.querySelector('#results').innerHTML = 'WIN'
-        document.querySelector('#pChoice').innerHTML = selection.name
-        document.querySelector('#cChoice').innerHTML = opponent.name
-        }
+    if(playerScore >= 5){
+        document.querySelector('#pChoice').innerHTML = 'You WIN'
+        document.querySelector('#cChoice').innerHTML = 'Refresh to play again'
     }
-    else if(selection.name == 'paper'){
-        if(opponent.name == 'scissors'){
-        compScore++
-        document.querySelector('#comp').innerHTML = 'Computer Score:' + ' ' + compScore
-        document.querySelector('#results').innerHTML = 'LOOSE'
-        document.querySelector('#pChoice').innerHTML = selection.name
-        document.querySelector('#cChoice').innerHTML = opponent.name
-        }else{
-        playerScore++
-        document.querySelector('#player').innerHTML = 'Player Score:' + ' ' + playerScore
-        document.querySelector('#results').innerHTML = 'WIN'
-        document.querySelector('#pChoice').innerHTML = selection.name
-        document.querySelector('#cChoice').innerHTML = opponent.name
-        }
+    else if(compScore >= 5){
+        document.querySelector('#pChoice').innerHTML = 'You LOSE'
+        document.querySelector('#cChoice').innerHTML = 'Refresh to play again'
     }
 }
     isWinner(playerSelection(num), computerSelection())
